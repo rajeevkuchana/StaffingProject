d2f2696ced868668b325666eedc7ebf5530c0351
Rajeev <rajeevjskuchana@gmail.com>
2024-08-11 17:54:49 +0530

    Updating the Stubs

--- src/main/java/com/staffing/jobportal/controller/ProfileController.java
+++ src/main/java/com/staffing/jobportal/controller/ProfileController.java
@@ -1,9 +1,7 @@
 package com.staffing.jobportal.controller;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -21,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.staffing.jobportal.models.JobProfiles;
+import com.staffing.jobportal.models.JobProfilesSubCat;
 import com.staffing.jobportal.models.ProfileDetails;
 import com.staffing.jobportal.models.ProfileSummary;
 import com.staffing.jobportal.models.SearchJob;
@@ -31,7 +30,6 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import springfox.documentation.annotations.ApiIgnore;
 
 @CrossOrigin(origins = { "http://localhost:3000" }, methods = { RequestMethod.OPTIONS, RequestMethod.GET,
 		RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.POST })
@@ -54,7 +52,64 @@ public class ProfileController {
 			@ApiParam(value = "Profile object to be added", required = true) @RequestBody SearchJob searchJob
 //            @RequestParam(value = "status") ProfileStatus status
 	) {
-		List<ProfileSummary> profileList = profileService.getAllProfiles(searchJob);
+
+		List<ProfileSummary> profileList = new ArrayList<ProfileSummary>();
+
+		ProfileSummary profileSummary = new ProfileSummary();
+		profileSummary.setProfileId("ee728662-7693-4444-ab1d-a84ffcdff8df");
+		profileSummary.setFirstName("Sachin");
+		profileSummary.setLastName("Ten");
+		profileSummary.setCurrentCompany("TCS");
+		profileSummary.setDesignation("Developer");
+		profileSummary.setCurrentCTC("23-LPA");
+		profileSummary.setExpectedCTC("30-LPA");
+		profileSummary.setOverallExp("10-Years");
+		profileSummary.setRelevantExp("8-Years");
+		profileSummary.setOverAllRating(4);
+		profileSummary.setLocation("Banglore");
+		profileList.add(profileSummary);
+
+		ProfileSummary profileSummary1 = new ProfileSummary();
+		profileSummary1.setProfileId("67dacf62-c323-4929-b44d-21985a362bc3");
+		profileSummary1.setFirstName("SachinA");
+		profileSummary1.setLastName("TenA");
+		profileSummary1.setCurrentCompany("WIPRO");
+		profileSummary1.setDesignation("Sr Developer");
+		profileSummary1.setCurrentCTC("35-LPA");
+		profileSummary1.setExpectedCTC("40-LPA");
+		profileSummary1.setOverallExp("15-Years");
+		profileSummary1.setRelevantExp("12-Years");
+		profileSummary1.setLocation("Mumbai");
+		profileSummary1.setOverAllRating(3);
+		profileList.add(profileSummary1);
+
+		ProfileSummary profileSummary2 = new ProfileSummary();
+		profileSummary2.setProfileId("831b4047-c10a-4642-8b7b-59de8df89f38");
+		profileSummary2.setFirstName("Rahul");
+		profileSummary2.setLastName("TenA");
+		profileSummary2.setCurrentCompany("TCS");
+		profileSummary2.setDesignation("DevOps");
+		profileSummary2.setCurrentCTC("25-LPA");
+		profileSummary2.setExpectedCTC("30-LPA");
+		profileSummary2.setOverallExp("5-Years");
+		profileSummary2.setRelevantExp("5-Years");
+		profileSummary2.setLocation("Hyderabad");
+		profileSummary2.setOverAllRating(3);
+		profileList.add(profileSummary2);
+
+		ProfileSummary profileSummary3 = new ProfileSummary();
+		profileSummary3.setProfileId("831b4047-c10a-4642-8b7b-59de8df89f38");
+		profileSummary3.setFirstName("Ramesh");
+		profileSummary3.setLastName("TenA");
+		profileSummary3.setCurrentCompany("TEST");
+		profileSummary3.setDesignation("QA");
+		profileSummary3.setCurrentCTC("17-LPA");
+		profileSummary3.setExpectedCTC("23-LPA");
+		profileSummary3.setOverallExp("9-Years");
+		profileSummary3.setRelevantExp("8-Years");
+		profileSummary3.setOverAllRating(3);
+		profileList.add(profileSummary3);
+
 		return profileList;
 
 	}
@@ -87,7 +142,105 @@ public class ProfileController {
 	@ApiOperation("Get a profile by ID")
 	public ResponseEntity<List<JobProfiles>> getJobProfiles(
 			@ApiParam(value = "Profile ID", example = "1", required = true) @RequestParam String jobCategory) {
-		List<JobProfiles> jobProfiles = profileService.getJobProfiles(jobCategory);
+		List<JobProfiles> jobProfiles = new ArrayList<JobProfiles>();
+
+		JobProfiles jobProfile = new JobProfiles();
+		jobProfile.setId("sedfvfsddfsf");
+		jobProfile.setCategoryCode("Cloud");
+		jobProfile.setCategoryDisplay("Cloud");
+		jobProfile.setCategoryIcon("https://i.imgur.com/6NKPrhO.png");
+		
+		List<JobProfilesSubCat> jobProfilesSubCats = new ArrayList<JobProfilesSubCat>(); 
+		
+		JobProfilesSubCat jobProfilesSubCat = new JobProfilesSubCat(); 
+		jobProfilesSubCat.setSubCategoryCode("AzureDE");
+		jobProfilesSubCat.setSubCategoryDisplay("Azure Data Engineer");
+		jobProfilesSubCat.setSubCategoryDesc("Description Azure Data Engineer");
+		jobProfilesSubCat.setSubCategoryIcon("https://i.imgur.com/6NKPrhO.png");
+		jobProfilesSubCats.add(jobProfilesSubCat);
+		
+		JobProfilesSubCat jobProfilesSubCat1 = new JobProfilesSubCat(); 
+		jobProfilesSubCat1.setSubCategoryCode("AWSDE");
+		jobProfilesSubCat1.setSubCategoryDisplay("AWS Data Engineer");
+		jobProfilesSubCat1.setSubCategoryDesc("Description AWS Data Engineer");
+		jobProfilesSubCat1.setSubCategoryIcon("https://i.imgur.com/6NKPrhO.png");
+		jobProfilesSubCats.add(jobProfilesSubCat1);
+		
+		JobProfilesSubCat jobProfilesSubCat2 = new JobProfilesSubCat(); 
+		jobProfilesSubCat2.setSubCategoryCode("GCPDE");
+		jobProfilesSubCat2.setSubCategoryDisplay("GCP Data Engineer");
+		jobProfilesSubCat2.setSubCategoryDesc("Description GCP  Data Engineer");
+		jobProfilesSubCat2.setSubCategoryIcon("https://i.imgur.com/6NKPrhO.png");
+		jobProfilesSubCats.add(jobProfilesSubCat2);
+		
+		JobProfilesSubCat jobProfilesSubCat3 = new JobProfilesSubCat(); 
+		jobProfilesSubCat3.setSubCategoryCode("ANYCDE");
+		jobProfilesSubCat3.setSubCategoryDisplay("ANY Cloud Data Engineer");
+		jobProfilesSubCat3.setSubCategoryDesc("Description ANY  Data Engineer");
+		jobProfilesSubCat3.setSubCategoryIcon("https://i.imgur.com/6NKPrhO.png");
+		jobProfilesSubCats.add(jobProfilesSubCat3);
+		
+		JobProfilesSubCat jobProfilesSubCat4 = new JobProfilesSubCat(); 
+		jobProfilesSubCat4.setSubCategoryCode("MULTICDE");
+		jobProfilesSubCat4.setSubCategoryDisplay("MULTI Data Engineer");
+		jobProfilesSubCat4.setSubCategoryDesc("Description MULTI Data Engineer");
+		jobProfilesSubCat4.setSubCategoryIcon("https://i.imgur.com/6NKPrhO.png");
+		jobProfilesSubCats.add(jobProfilesSubCat4);
+		
+		JobProfilesSubCat jobProfilesSubCat5 = new JobProfilesSubCat(); 
+		jobProfilesSubCat5.setSubCategoryCode("NOCDE");
+		jobProfilesSubCat5.setSubCategoryDisplay("NO Data Engineer");
+		jobProfilesSubCat5.setSubCategoryDesc("Description NO Data Engineer");
+		jobProfilesSubCat5.setSubCategoryIcon("https://i.imgur.com/6NKPrhO.png");
+		jobProfilesSubCats.add(jobProfilesSubCat5);
+		
+		jobProfile.setJobProfilesSubCats(jobProfilesSubCats);
+		jobProfiles.add(jobProfile);
+		
+		
+		JobProfiles jobProfile1 = new JobProfiles();
+		jobProfile1.setId("sedfvfsddfsf");
+		jobProfile1.setCategoryCode("DataAI");
+		jobProfile1.setCategoryDisplay("DATA & AI");
+		jobProfile1.setCategoryIcon("https://i.imgur.com/6NKPrhO.png");
+		jobProfiles.add(jobProfile1);
+		
+		JobProfiles jobProfile2 = new JobProfiles();
+		jobProfile2.setId("sedfvfsddfsf");
+		jobProfile2.setCategoryCode("FullStack");
+		jobProfile2.setCategoryDisplay("Full Stack");
+		jobProfile2.setCategoryIcon("https://i.imgur.com/6NKPrhO.png");
+		jobProfiles.add(jobProfile2);
+		
+		JobProfiles jobProfile3 = new JobProfiles();
+		jobProfile3.setId("sedfvfsddfsf");
+		jobProfile3.setCategoryCode("QA");
+		jobProfile3.setCategoryDisplay("Quality Assurance");
+		jobProfile3.setCategoryIcon("https://i.imgur.com/6NKPrhO.png");
+		jobProfiles.add(jobProfile3);
+		
+		JobProfiles jobProfile4 = new JobProfiles();
+		jobProfile4.setId("sedfvfsddfsf");
+		jobProfile4.setCategoryCode("QA");
+		jobProfile4.setCategoryDisplay("Quality Assurance");
+		jobProfile4.setCategoryIcon("https://i.imgur.com/6NKPrhO.png");
+		jobProfiles.add(jobProfile4);
+		
+		JobProfiles jobProfile5 = new JobProfiles();
+		jobProfile5.setId("sedfvfsddfsf");
+		jobProfile5.setCategoryCode("CRM");
+		jobProfile5.setCategoryDisplay("Customer Relationship Management");
+		jobProfile5.setCategoryIcon("https://i.imgur.com/6NKPrhO.png");
+		jobProfiles.add(jobProfile5);
+
+		JobProfiles jobProfile6 = new JobProfiles();
+		jobProfile6.setId("sedfvfsddfsf");
+		jobProfile6.setCategoryCode("DevOps");
+		jobProfile6.setCategoryDisplay("Development and Operation");
+		jobProfile6.setCategoryIcon("https://i.imgur.com/6NKPrhO.png");
+		jobProfiles.add(jobProfile6);
+		
+		
 		return ResponseEntity.status(HttpStatus.CREATED).body(jobProfiles);
 	}
 
@@ -101,9 +254,60 @@ public class ProfileController {
 
 	@GetMapping("/clientSelected")
 	@ApiOperation("Get profiles with selected status by client")
-	public List<ProfileDetails> getProfilesClientSelected(
+	public List<ProfileSummary> getProfilesClientSelected(
 			@ApiParam(value = "email", required = true) @RequestParam(value = "email") String email) {
-		List<ProfileDetails> profileList = profileService.getProfilesClientSelected(email);
+		List<ProfileSummary> profileList = new ArrayList<ProfileSummary>();
+
+		ProfileSummary profileSummary = new ProfileSummary();
+
+		profileSummary.setProfileId("ee728662-7693-4444-ab1d-a84ffcdff8df");
+		profileSummary.setFirstName("Sachin");
+		profileSummary.setLastName("Ten");
+		profileSummary.setCurrentCompany("TCS");
+		profileSummary.setDesignation("Developer");
+		profileSummary.setCurrentCTC("23-LPA");
+		profileSummary.setExpectedCTC("30-LPA");
+		profileSummary.setOverallExp("10-Years");
+		profileSummary.setRelevantExp("8-Years");
+		profileSummary.setOverAllRating(4);
+		profileList.add(profileSummary);
+
+		profileSummary.setProfileId("67dacf62-c323-4929-b44d-21985a362bc3");
+		profileSummary.setFirstName("SachinA");
+		profileSummary.setLastName("TenA");
+		profileSummary.setCurrentCompany("WIPRO");
+		profileSummary.setDesignation("Sr Developer");
+		profileSummary.setCurrentCTC("35-LPA");
+		profileSummary.setExpectedCTC("40-LPA");
+		profileSummary.setOverallExp("15-Years");
+		profileSummary.setRelevantExp("12-Years");
+		profileSummary.setOverAllRating(3);
+		profileList.add(profileSummary);
+
+		profileSummary.setProfileId("831b4047-c10a-4642-8b7b-59de8df89f38");
+		profileSummary.setFirstName("Rahul");
+		profileSummary.setLastName("TenA");
+		profileSummary.setCurrentCompany("TCS");
+		profileSummary.setDesignation("DevOps");
+		profileSummary.setCurrentCTC("25-LPA");
+		profileSummary.setExpectedCTC("30-LPA");
+		profileSummary.setOverallExp("5-Years");
+		profileSummary.setRelevantExp("5-Years");
+		profileSummary.setOverAllRating(3);
+		profileList.add(profileSummary);
+
+		profileSummary.setProfileId("831b4047-c10a-4642-8b7b-59de8df89f38");
+		profileSummary.setFirstName("Ramesh");
+		profileSummary.setLastName("TenA");
+		profileSummary.setCurrentCompany("TEST");
+		profileSummary.setDesignation("QA");
+		profileSummary.setCurrentCTC("17-LPA");
+		profileSummary.setExpectedCTC("23-LPA");
+		profileSummary.setOverallExp("9-Years");
+		profileSummary.setRelevantExp("8-Years");
+		profileSummary.setOverAllRating(3);
+		profileList.add(profileSummary);
+
 		return profileList;
 	}
 
--- src/main/java/com/staffing/jobportal/controller/UserController.java
+++ src/main/java/com/staffing/jobportal/controller/UserController.java
@@ -49,13 +49,23 @@ public class UserController {
 		return ResponseEntity.status(HttpStatus.OK).body(deleteStatus);
 	}
 
+
 	@PostMapping("/user/verify")
 	public ResponseEntity<User> verifyUser(
 			@ApiParam(value = "Profile object to be added", required = true) @RequestBody UserLogin userLogin) {
-		User user = userService.verifyUser(userLogin.getEmail(), userLogin.getPassword());
+		User user = new User();
+		user.setCompany("Wipro");
+		user.setEmail("test@test.com");
+		user.setId("dfsdfsdffsdfsdfsdfsd");
+		user.setPassword("password");
+		user.setRole("Client");
+		user.setUsername("Sample Test");
+	
 		return ResponseEntity.status(HttpStatus.OK).body(user);
 
 	}
+
+
 
 	@GetMapping("users")
 	public ResponseEntity<List<User>> getAllUser() {
