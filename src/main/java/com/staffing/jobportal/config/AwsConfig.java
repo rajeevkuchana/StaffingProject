a183f947aa142840848ad8299ef2986e6bcde97e
Rajeev <rajeevjskuchana@gmail.com>
2024-11-10 19:10:41 +0530

    updated AS+WS Config file

--- src/main/java/com/staffing/jobportal/config/AwsConfig.java
+++ src/main/java/com/staffing/jobportal/config/AwsConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -21,8 +22,8 @@ public class AwsConfig {
 
     @Bean
     public S3Client s3Client() {
-        return S3Client.builder()
-                .region(Region.of(region)) // Replace with your bucket's region
+        return S3Client.builder().credentialsProvider(DefaultCredentialsProvider.create())
+                .region(Region.of(region))
                 .build();
     }
 }
